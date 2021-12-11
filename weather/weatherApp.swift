9624c95a830663826878be82fa616dcbbbdb86af
Ricardo <ricardo.l.jardim@hotmail.com>
2021-12-11 19:00:09 +0000

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Ricardo Lucas on 11/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
